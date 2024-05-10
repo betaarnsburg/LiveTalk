20b5105573dcea1f21bd29b1e69f821554825f3c
betaarnsburg <64945891+betaarnsburg@users.noreply.github.com>
2024-05-10 16:34:48 +0200

    user

---
+++ backend/src/main/java/com/example/backend/entities/Message.java
@@ -0,0 +1,24 @@
+package com.example.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private LocalDateTime createdAt;
+    @ManyToOne
+    private User user;
+}
---
+++ backend/src/main/java/com/example/backend/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.backend.repositories;
+
+import com.example.backend.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findByLastName(String lastName);
+}
